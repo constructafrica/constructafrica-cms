d379bcbd442e036b3d0d07f7cd866f82345e62ac
mrprotocoll <lekanvgbg@gmail.com>
2025-12-24 21:34:05 +0100

    update

--- src-extensions/src/endpoints/projects/index.js
+++ src-extensions/src/endpoints/projects/index.js
@@ -745,7 +745,8 @@ export default (router, { services, exceptions, getSchema, database}) => {
                     'contacts.company_contacts_id.linkedin',
                     'contacts.company_contacts_id.photo.*',
                     'featured_image.*',
-                    'news.*'
+                    'news.*',
+                    'image_gallery.*'
                 ]
             });
 
@@ -814,6 +815,30 @@ export default (router, { services, exceptions, getSchema, database}) => {
             } else {
                 project.contacts = [];
             }
+
+            // if (project.image_gallery && Array.isArray(project.image_gallery)) {
+            //     project.image_gallery = project.image_gallery
+            //         .filter(pc => pc.company_contacts_id)
+            //         .map(pc => ({
+            //             id: pc.company_contacts_id.id,
+            //             name: pc.company_contacts_id.name || null,
+            //             caption: pc.company_contacts_id.name || null,
+            //             alt_text: pc.company_contacts_id.name || null,
+            //             photo: {
+            //                 id: pc.company_contacts_id.company.id || null,
+            //                 name: pc.company_contacts_id.company.name || null,
+            //                 email: pc.company_contacts_id.company.email || null,
+            //             },
+            //             email: pc.company_contacts_id.email || null,
+            //             phone: pc.company_contacts_id.phone || null,
+            //             role: pc.company_contacts_id.role || null,
+            //             linkedin: pc.company_contacts_id.linkedin || null,
+            //             twitter: pc.company_contacts_id.twitter || null,
+            //             facebook: pc.company_contacts_id.facebook || null,
+            //         }));
+            // } else {
+            //     project.image_gallery = [];
+            // }
 
             // Handle favorites
             let is_favorited = false;
